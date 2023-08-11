84f99d561d17a87b28593ff560f3b9f575fd1f56
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-08-11 10:22:57 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function fecthPost(){
+    const url = 'https://api-rest-post-diegocandido.herokuapp.com/postagens/';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        mostrarPosts(data);
+    })
+    .catch(error => console.error('Erro ao buscar postagem', error));
+}
+function mostrarPosts(posts){
+    const containerPosts = document.getElementById('posts');
+    posts.forEach(post => {
+        const thumbImageSrc = post.thumbImage.startsWith('/') ? `https://api-rest-post-diegocandido.herokuapp.com${post.thumbImage}` : post.thumbImage;
+        const profileThumbImageSrc = post.profileThumbImage.startsWith('/') ? `https://api-rest-post-diegocandido.herokuapp.com${post.profileThumbImage}` : post.profileThumbImage;
+        const postHTML = `
+        <div id ="post">
+        <img id="thumbImage" src="${thumbImageSrc}" alt="${post.thumbImageAltText}">
+            <div id="cimaPost">
+                <h2>${post.title}</h2>
+                <p>${post.description}</p>
+                <p>Por ${post.profileName}, ${post.postDate}</p>
+            </div>
+        </div>`;
+        containerPosts.innerHTML += postHTML;
+    });
+}
+fecthPost();
